5387184815c5eb9a3046a689fd8c056cc80ce721
pidhirnyi <vitalikpidhirniy@gmail.com>
2023-08-21 15:53:55 +0300

    Add file

---
+++ task2.py
@@ -0,0 +1,21 @@
+# Hello world
+print("Hello, World!")
+
+# The 'end' parameter
+print("Hello, World", end="!" "\n")
+
+# The 'sep' parameter
+print("Hello", "World!", sep=", ")
+
+# Capital letters "O" and "H"
+print("#########\n"
+      "#\t\t#\n"
+      "#\t\t#\n"
+      "#\t\t#\n"
+      "#########\n"
+      "\n"
+      "#\t\t#\n"
+      "#\t\t#\n"
+      "#########\n"
+      "#\t\t#\n"
+      "#\t\t#")
