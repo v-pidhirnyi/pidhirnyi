5970dd3269720b22d6c95c433c76488d1f61f35c
pidhirnyi <vitalikpidhirniy@gmail.com>
2023-08-25 23:26:09 +0300

    Add file

---
+++ task3.py
@@ -0,0 +1,30 @@
+# The greeting program
+name = "Vitalii"
+day = "Thursday"
+print("Good day, " + str(name) + "!")
+print(str(day), "is a perfect day to learn some python.")
+
+# Manipulate strings
+my_name = "Vitalii"
+my_last_name = "Pidhirnyi"
+print("Hello,", my_name, my_last_name)
+
+# Using python as a calculator
+num1 = 12
+num2 = 3
+
+addition = num1 + num2
+subtraction = num1 - num2
+division = num1 / num2
+multiplication = num1 * num2
+exponent = num1 ** num2
+modulus = num1 % num2
+floor_division = num1 // num2
+
+print("Sum of", num1, "and", num2 , "is", addition)
+print("Subtraction of", num1, "and", num2, "is", subtraction)
+print("Division of", num1, "and", num2, "is", division)
+print("Multiplication of", num1, "and", num2, "is", multiplication)
+print("Exponent of", num1, "and", num2, "is", exponent)
+print("Modulus of", num1, "and", num2, "is", modulus)
+print("Floor division of", num1, "and", num2, "is", floor_division)
